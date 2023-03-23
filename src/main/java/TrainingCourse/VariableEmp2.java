ab171444aa765aee937685cac7d0565e4a6802d1
mudas <mudas@hp>
2023-03-23 07:18:20 +0530

    Variable example 2

---
+++ src/main/java/TrainingCourse/VariableEmp2.java
@@ -0,0 +1,17 @@
+package TrainingCourse;
+class Calculator {
+	void add() {
+		int a=10;
+		int b=20;
+		
+		int res=a+b;
+		System.out.println(res);
+	}
+}
+public class VariableEmp2 {
+       public static void main(String[] args) {
+    	   Calculator c1=new Calculator();
+    	   System.out.println(c1);
+    	   c1.add();
+       }
+}
